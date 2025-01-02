400249c333cc519f60f3f9b497dc6ad9345e85d3
Collapsar <collapsar0615@gmail.com>
2025-01-02 23:36:06 +0800

    01022336

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,33 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1735831718',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/timeline/',
+      
+        '/about/',
+      
+        '/links/',
+      
+
+      
+      
+        '/assets/js/dist/app.min.js',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
