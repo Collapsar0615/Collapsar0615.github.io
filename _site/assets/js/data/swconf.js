e40a2b0d642e32dd78d597044ffbe9eab7672416
Collapsar <collapsar0615@gmail.com>
2025-01-16 21:06:50 +0800

    finaltest

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1737032434',resources: [
+    cacheName: 'chirpy-1737032803',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
