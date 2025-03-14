e4af4418cdf32c9593bd2ce189b311a3763aec70
Ma Tao <142155083+Collapsar0615@users.noreply.github.com>
2025-03-14 20:21:28 +0800

    03142021

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741954672',resources: [
+    cacheName: 'chirpy-1741954872',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
